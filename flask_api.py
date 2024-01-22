ddc2e1dc50400c4f1ce388f31d2c959f0de35ee1
LenartR <lenart.ribnikar@gmail.com>
2024-01-22 12:18:57 +0100

    Aded code to flask api

--- flask_api.py
+++ flask_api.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
+    #add comments
+for i in range([1, 5]):
+    print(i)
+    if i == 4:
+        print('Hi i am the formth i!')
